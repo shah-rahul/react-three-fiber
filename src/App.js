f34f279ae2c08bcf8e101f07f2a2cc9f567e302c
rahul <shahrahul795@gmail.com>
2020-12-03 21:13:48 +0530

    first build

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import { Canvas } from 'react-three-fiber';
+import { useSpring, a } from 'react-spring/three';
+
+const Box = () => {
+  const [hovered, setHovered] = useState(false);
+  const [active, setActive] = useState(false);
+  const props = useSpring({
+    scale: active ? [5, 5, 5] : [1, 1, 1],
+    color: hovered ? 'pink' : 'grey',
+  });
+
+  return (
+    <a.mesh
+      onPointerOver={() => setHovered(true)}
+      onPointerOut={() => setHovered(false)}
+      onClick={() => setActive(!active)}
+      scale={props.scale}>
+      <boxBufferGeometry attach='geometry' args={[1, 1, 1]} />
+      <a.meshBasicMaterial attach='material' color={props.color} />
+    </a.mesh>
+  );
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Canvas id='root'>
+      <Box />
+    </Canvas>
   );
 }
 
